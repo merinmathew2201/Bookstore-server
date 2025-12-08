d68d8a5c86ab12a0e5b03fab0b3e78b3f0e1b36b
merinmathew2201 <merinmathewsuni@gmail.com>
2025-12-08 15:35:45 +0530

    login & register completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const users = require('../model/userModel')
+const jwt = require('jsonwebtoken')
+
 // register
 exports.registerController =async (req,res)=>{
     console.log("Inside register controller");
@@ -23,5 +25,26 @@ exports.registerController =async (req,res)=>{
     
 }
 // login
+exports.loginController = async (req,res)=>{
+    console.log("Inside loginController");
+    const {email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            if(password == existingUser.password){
+                const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.jwtSecret)
+                res.status(200).json({user:existingUser,token})
+            }else{
+                res.status(401).json("Incorrect email/password!!!")
+            }
+        }else{
+            res.status(404).json("Account Doesnot exists!!!")
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)
+    }
+    
+}
 // user profile edit
 // admin profile edit
--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,8 @@ const router = new express.Router()
 // register
 router.post('/register',userController.registerController)
 
+// login
+router.post('/login',userController.loginController)
+
 
 module.exports = router
