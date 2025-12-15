ee0d9e711dd18ca8390016e6ba6be4575eb39125
merinmathew2201 <merinmathewsuni@gmail.com>
2025-12-15 15:32:11 +0530

    added logic for book upload

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,5 +3,25 @@ const books = require('../model/bookModel')
 // add book
 exports.addBookController = async (req,res)=>{
     console.log("Inside addBookController");
-    res.status(200).json("addBook request received!!!!")
+    // get data from req body
+    const {title,author,pages,imageURL,price,discountPrice,abstract,publisher,language,isbn,category} = req.body
+    const uploadImg = req.files.map(item=>item.filename)
+    const sellerMail = req.payload
+    console.log(title,author,pages,imageURL,price,discountPrice,abstract,publisher,language,isbn,category,uploadImg,sellerMail);
+    
+    try{
+        const existingBook = await books.findOne({title,sellerMail})
+        if(existingBook){
+            res.status(401).json("Book already exists!!! Request Failed...")
+        }else{
+            const newBook = await books.create({
+                title,author,pages,imageURL,price,discountPrice,abstract,publisher,language,isbn,category,uploadImg,sellerMail
+            })
+            res.status(200).json(newBook)
+        }
+
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error)   
+    }
 } 
--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userController = require('../controllers/userController')
 const bookController = require('../controllers/bookController')
 const jwtMiddleware = require('../middlewares/jwtMiddleware')
+const multerMiddleware = require('../middlewares/multerMiddleware')
 
 const router = new express.Router()
 
@@ -15,8 +16,8 @@ router.post('/login',userController.loginController)
 router.post('/google-login',userController.googleLoginController)
 
 // --------Authorised user
-// add book
-router.post('/user/add/book',jwtMiddleware,bookController.addBookController)
+// add book - request body in formdata,header should has token
+router.post('/user/add/book',jwtMiddleware,multerMiddleware.array('uploadImg',3) ,bookController.addBookController)
 
 
 module.exports = router
